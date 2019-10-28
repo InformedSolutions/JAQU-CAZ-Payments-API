365f820029d483d98ee7fca7ecb3b0602f5ee045
jaroslaw <jaroslaw.piotrowski@pattern-match.com>
2019-10-28 12:35:27 +0100

    VCCS-692 update lambda initialization

--- src/main/java/uk/gov/caz/psr/amazonaws/StreamLambdaHandler.java
+++ src/main/java/uk/gov/caz/psr/amazonaws/StreamLambdaHandler.java
@@ -1,39 +1,48 @@
 package uk.gov.caz.psr.amazonaws;
 
+import static uk.gov.caz.awslambda.AwsHelpers.splitToArray;
+
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.model.ContainerConfig;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import com.google.common.base.Splitter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import uk.gov.caz.Application;
+import java.time.Instant;
+import uk.gov.caz.psr.Application;
 
 public class StreamLambdaHandler implements RequestStreamHandler {
-
-  private static final Splitter SPLITTER =
-      Splitter.on(',').trimResults().omitEmptyStrings();
 
   private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
 
   static {
+    long startTime = Instant.now().toEpochMilli();
     try {
-      String listOfActiveSpringProfiles =
-          System.getenv("SPRING_PROFILES_ACTIVE");
+      // For applications that take longer than 10 seconds to start, use the async builder:
+      String listOfActiveSpringProfiles = System.getenv("SPRING_PROFILES_ACTIVE");
+      ContainerConfig.defaultConfig().setInitializationTimeout(20_000);
       if (listOfActiveSpringProfiles != null) {
-        handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(
-            Application.class, splitToArray(listOfActiveSpringProfiles));
+        handler = new SpringBootProxyHandlerBuilder()
+            .defaultProxy()
+            .asyncInit(startTime)
+            .springBootApplication(Application.class)
+            .profiles(splitToArray(listOfActiveSpringProfiles))
+            .buildAndInitialize();
       } else {
-        handler = SpringBootLambdaContainerHandler
-            .getAwsProxyHandler(Application.class);
+        handler = new SpringBootProxyHandlerBuilder()
+            .defaultProxy()
+            .asyncInit(startTime)
+            .springBootApplication(Application.class)
+            .buildAndInitialize();
       }
     } catch (ContainerInitializationException e) {
       // if we fail here. We re-throw the exception to force another cold start
-      throw new RuntimeException("Could not initialize Spring Boot application",
-          e);
+      throw new RuntimeException("Could not initialize Spring Boot application", e);
     }
   }
 
@@ -41,9 +50,5 @@ public class StreamLambdaHandler implements RequestStreamHandler {
   public void handleRequest(InputStream inputStream, OutputStream outputStream,
       Context context) throws IOException {
     handler.proxyStream(inputStream, outputStream, context);
-  }
-
-  private static String[] splitToArray(String activeProfiles) {
-    return SPLITTER.splitToList(activeProfiles).toArray(new String[0]);
   }
 }
