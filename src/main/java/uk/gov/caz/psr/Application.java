d14fe1cea9b8d3c95d7e4f97c4788ccab2074b06
Radoslaw Morytko <radoslaw.morytko@informed.com>
2019-11-06 09:17:13 +0100

    NON-JIRA: Increase application's initialization timeout from 20 to 25 seconds
    * narrow down the scanned base packages

--- src/main/java/uk/gov/caz/psr/Application.java
+++ src/main/java/uk/gov/caz/psr/Application.java
@@ -3,7 +3,12 @@ package uk.gov.caz.psr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "uk.gov.caz.psr.configuration",
+    "uk.gov.caz.psr.controller",
+    "uk.gov.caz.psr.repository",
+    "uk.gov.caz.psr.service"
+})
 public class Application {
 
   public static void main(String[] args) {
