b3c69a00af28b514eb2d3dd60747e205bfca2a19
Radoslaw Morytko <radoslaw.morytko@informed.com>
2019-11-12 16:12:10 +0100

    NON-JIRA Fix inserting duplicate vehicle entrant entities
    * restore the version with `ON CONFLICT DO NOTHING` statement
    * add integration and unit tests

--- src/it/java/uk/gov/caz/psr/SuccessAddingVehicleEntrantIT.java
+++ src/it/java/uk/gov/caz/psr/SuccessAddingVehicleEntrantIT.java
@@ -70,6 +70,14 @@ public class SuccessAddingVehicleEntrantIT {
         .vehicleEntrantIsCreatedInDatabase()
         .vehicleEntrantIsNotConnectedToVehicleEntrantPayments();
 
+    // a request for the same day, vrn and CAZ is made again
+    given()
+        .vehicleEntrantRequest(vehicleEntrantRequest(notPaidDay))
+        .whenSubmitted()
+        .then()
+        .vehicleEntrantIsFoundInDatabase()
+        .vehicleEntrantIsNotConnectedToVehicleEntrantPayments();
+
     given()
         .vehicleEntrantRequest(vehicleEntrantRequest(paidDay))
         .whenSubmitted()
@@ -172,6 +180,10 @@ public class SuccessAddingVehicleEntrantIT {
               + "payment_status = 'PAID' AND "
               + "vehicle_entrant_id is not null");
       assertThat(vehicleEntrantPaymentCount).isEqualTo(1);
+    }
+
+    public VehicleEntrantJourneyAssertion vehicleEntrantIsFoundInDatabase() {
+      return vehicleEntrantIsCreatedInDatabase();
     }
   }
 }
