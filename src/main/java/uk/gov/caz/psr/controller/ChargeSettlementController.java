ebf98c421a62b4c1e5516afff60b6b9fb845bdd7
Antoni Pstraś <antek.pstras@gmail.com>
2019-11-13 16:29:55 +0100

    [CAZ-1416] GET ChargeSettlement endpoint with fake data

--- src/main/java/uk/gov/caz/psr/controller/ChargeSettlementController.java
+++ src/main/java/uk/gov/caz/psr/controller/ChargeSettlementController.java
@@ -3,7 +3,9 @@ package uk.gov.caz.psr.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.caz.psr.dto.ChargeSettlementPaymentStatus;
 import uk.gov.caz.psr.dto.PaymentInfoResponse;
+import uk.gov.caz.psr.dto.PaymentStatusRequest;
 import uk.gov.caz.psr.dto.PaymentStatusResponse;
 import uk.gov.caz.psr.dto.PaymentStatusUpdateRequest;
 import uk.gov.caz.psr.dto.PaymentUpdateSuccessResponse;
@@ -25,8 +27,22 @@ public class ChargeSettlementController implements ChargeSettlementControllerApi
   }
 
   @Override
-  public ResponseEntity<PaymentStatusResponse> getPaymentStatus() {
-    throw new UnsupportedOperationException();
+  public ResponseEntity<PaymentStatusResponse> getPaymentStatus(PaymentStatusRequest request) {
+    /* TODO: Fetch:
+     *    paymentStatus from VEHICLE_ENTRANT_PAYMENT table
+     *    paymentId from PAYMENT table
+     *    paymentStatus from VEHICLE_ENTRANT_PAYMENT table
+     *
+     **/
+    log.info("Received request: {}", request);
+
+    PaymentStatusResponse fakeResponse = PaymentStatusResponse.builder()
+        .chargeSettlementPaymentStatus(ChargeSettlementPaymentStatus.PAID)
+        .paymentId("350be6da-10f1-41fe-9840-98c738ec763e")
+        .caseReference("sample-case-reference")
+        .build();
+
+    return ResponseEntity.ok(fakeResponse);
   }
 
   @Override
