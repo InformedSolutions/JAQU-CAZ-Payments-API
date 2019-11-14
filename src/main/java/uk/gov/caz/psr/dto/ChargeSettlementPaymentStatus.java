9a5f4637d8d61719d71291075846440ccde3d139
Antoni Pstraś <antek.pstras@gmail.com>
2019-11-14 16:15:02 +0100

    [CAZ-1417] Repository layer for GET paymentStatus

--- src/main/java/uk/gov/caz/psr/dto/ChargeSettlementPaymentStatus.java
+++ src/main/java/uk/gov/caz/psr/dto/ChargeSettlementPaymentStatus.java
@@ -1,8 +1,19 @@
 package uk.gov.caz.psr.dto;
 
+import uk.gov.caz.psr.model.InternalPaymentStatus;
+
 public enum ChargeSettlementPaymentStatus {
   PAID,
   NOT_PAID,
   REFUNDED,
-  CHARGEBACK
+  CHARGEBACK;
+
+  /**
+   * Maps {@link ChargeSettlementPaymentStatus} to {@link InternalPaymentStatus}.
+   */
+  public static ChargeSettlementPaymentStatus from(InternalPaymentStatus internalPaymentStatus) {
+    return ChargeSettlementPaymentStatus.valueOf(internalPaymentStatus.toString());
+  }
 }
+
+
