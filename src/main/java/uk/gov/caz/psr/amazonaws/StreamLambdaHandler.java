b01ccbe600c5229fa8693c3367a60cbf07561081
phan-anh-tuan <phan.anh.tuan@gmail.com>
2019-11-25 16:23:34 +1100

    add thundra warmup endpoint

--- src/main/java/uk/gov/caz/psr/amazonaws/StreamLambdaHandler.java
+++ src/main/java/uk/gov/caz/psr/amazonaws/StreamLambdaHandler.java
@@ -10,14 +10,34 @@ import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.Charset;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.util.StreamUtils;
+
 import uk.gov.caz.psr.Application;
 
+@Slf4j
 public class StreamLambdaHandler implements RequestStreamHandler {
 
+  private static final String KEEP_WARM_ACTION = "keep-warm";
   private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
 
   private static final int INITIALIZATION_TIMEOUT_IN_MS = 60_000;
@@ -44,8 +64,111 @@ public class StreamLambdaHandler implements RequestStreamHandler {
   }
 
   @Override
-  public void handleRequest(InputStream inputStream, OutputStream outputStream,
-      Context context) throws IOException {
-    handler.proxyStream(inputStream, outputStream, context);
+  public void handleRequest(final InputStream inputStream, final OutputStream outputStream,
+      final Context context)
+      throws IOException {
+    String input = StreamUtils.copyToString(inputStream, Charset.defaultCharset());
+    log.info("Input received: " + input);
+    if (isWarmupRequest(input)) {
+      delayToAllowAnotherLambdaInstanceWarming();
+      try (Writer osw = new OutputStreamWriter(outputStream)) {
+        osw.write(LambdaContainerStats.getStats());
+      }
+    } else {
+      LambdaContainerStats.setLatestRequestTime(LocalDateTime.now());
+      handler.proxyStream(new ByteArrayInputStream(input.getBytes()), outputStream, context);
+    }
+  }
+
+  /**
+   * Delay lambda response if the container is new and cold to allow subsequent
+   * keep-warm requests to be routed to a different lambda container.
+   * 
+   * @throws IOException when it is impossible to pause the thread
+   */
+  private void delayToAllowAnotherLambdaInstanceWarming() throws IOException {
+    try {
+      if (LambdaContainerStats.getLatestRequestTime() == null) {
+        int sleepDuration = Integer
+            .parseInt(Optional.ofNullable(
+                System.getenv("thundra_lambda_warmup_warmupSleepDuration"))
+                .orElse("100"));
+        log.info(String.format("Container %s go to sleep for %f seconds",
+                LambdaContainerStats.getInstanceId(),
+                (double) sleepDuration / 1000));
+        Thread.sleep(sleepDuration);
+      }
+    } catch (final Exception e) {
+      throw new IOException(e);
+    }
+  }
+
+  /**
+   * Determine if the incoming request is a keep-warm one.
+   * 
+   * @param action the request under examination
+   * @return true if the incoming request is a keep-warm one otherwise false.
+   */
+  private boolean isWarmupRequest(final String action) {
+    boolean isWarmupRequest = action.contains(KEEP_WARM_ACTION);
+    if (isWarmupRequest) {
+      log.info("Received lambda warmup request");
+    }
+    return isWarmupRequest;
+  }
+
+  /**
+   * Contain information about the lambda container.
+   */
+  static class LambdaContainerStats {
+    private static final String INSTANCE_ID = UUID.randomUUID().toString();
+    private static final DateTimeFormatter formatter = 
+                            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+    private static LocalDateTime latestRequestTime;
+
+    private LambdaContainerStats() {
+      throw new IllegalStateException("Statistic class");
+    }
+
+    /**
+     * Get container instanceId.
+     */
+    public static String getInstanceId() {
+      return INSTANCE_ID;
+    }
+
+    /**
+     * Get the container stats.
+     * 
+     * @return a string that contains lambda container Id and (optionally) the time
+     *         that the container last serve a request.
+     */
+    public static String getStats() {
+      try {
+        ObjectMapper obj = new ObjectMapper();
+        Map<String, String> retVal = new HashMap<>();
+        retVal.put("instanceId", INSTANCE_ID);
+        if (latestRequestTime != null) {
+          retVal.put("latestRequestTime",latestRequestTime.format(formatter));
+        }
+        return obj.writeValueAsString(retVal);
+      } catch (JsonProcessingException ex) {
+        return String.format("\"instanceId\": \"%s\"", INSTANCE_ID);
+      }
+    }
+
+    /**
+    * Get the time that the container last served a request.
+    */
+    public static LocalDateTime getLatestRequestTime() {
+      return latestRequestTime;
+    }
+
+    /**
+    * Set the time that the container serves a request.
+    */
+    public static void setLatestRequestTime(final LocalDateTime latestRequestTime) {
+      LambdaContainerStats.latestRequestTime = latestRequestTime;
+    }
   }
 }
