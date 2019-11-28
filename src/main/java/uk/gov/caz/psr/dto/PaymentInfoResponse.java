72c0ed42a02b7951dccd63d481ef747d8d512e75
Radoslaw Morytko <radoslaw.morytko@informed.com>
2019-11-28 17:30:29 +0100

    NON-JIRA Align `payment-info` response with the ICD v1.6

--- src/main/java/uk/gov/caz/psr/dto/PaymentInfoResponse.java
+++ src/main/java/uk/gov/caz/psr/dto/PaymentInfoResponse.java
@@ -1,8 +1,11 @@
 package uk.gov.caz.psr.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import lombok.Builder;
 import lombok.Value;
 
 /**
@@ -12,34 +15,48 @@ import lombok.Value;
 @Value
 public class PaymentInfoResponse {
   @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.results}")
-  List<PaymentInfoResults> results;
-
-  @Value
-  public static class PaymentInfoResults {
-    @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.vrn}")
-    String vrn;
-    @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.payments}")
-    List<PaymentsInfo> payments;
-  }
+  List<PaymentsInfo> results;
 
   @Value
   public static class PaymentsInfo {
-    @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.payment-id}")
-    String paymentId;
+    @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.vrn}")
+    String vrn;
+    @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.payments}")
+    List<SinglePaymentInfo> payments;
+  }
+
+  @Value
+  @Builder
+  public static class SinglePaymentInfo {
+    @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.payment-provider-id}")
+    String paymentProviderId;
 
     @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.payment-date}")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     LocalDate paymentDate;
 
-    @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.payment-status}")
-    ChargeSettlementPaymentStatus chargeSettlementPaymentStatus;
+    @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.total-paid}")
+    BigDecimal totalPaid;
 
-    @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.case-reference}")
-    String caseReference;
+    @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.line-items}")
+    List<VehicleEntrantPaymentInfo> lineItems;
 
-    @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.charge-paid}")
-    double chargePaid;
+    @Value
+    @Builder
+    public static class VehicleEntrantPaymentInfo {
 
-    @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.travel-dates}")
-    List<LocalDate> travelDates;
+      @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.payment-status}")
+      ChargeSettlementPaymentStatus chargeSettlementPaymentStatus;
+
+      @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.case-reference}")
+      String caseReference;
+
+      @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.charge-paid}")
+      BigDecimal chargePaid;
+
+      @ApiModelProperty(value = "${swagger.model.descriptions.payment-info.travel-date}")
+      @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+      LocalDate travelDate;
+    }
   }
 }
