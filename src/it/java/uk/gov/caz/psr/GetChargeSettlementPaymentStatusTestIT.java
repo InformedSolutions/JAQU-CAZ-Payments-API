260a10f6eea6e7062fd3e95ab77447fe6c6c9a2c
dratka <d.ratka@exlabs.pl>
2020-01-16 21:36:59 +0100

    [CAZ-1722] Fix GET ChargeSettelment PaymentStatus after Data staucture update

--- src/it/java/uk/gov/caz/psr/GetChargeSettlementPaymentStatusTestIT.java
+++ src/it/java/uk/gov/caz/psr/GetChargeSettlementPaymentStatusTestIT.java
@@ -2,13 +2,11 @@ package uk.gov.caz.psr;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import lombok.SneakyThrows;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -28,7 +26,6 @@ import uk.gov.caz.psr.model.PaymentStatus;
 import uk.gov.caz.psr.repository.PaymentStatusRepository;
 import uk.gov.caz.psr.util.TestObjectFactory.PaymentStatusFactory;
 
-@Disabled("Because of ERD updates")
 @Sql(scripts = "classpath:data/sql/add-payments-for-payment-status.sql",
     executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = "classpath:data/sql/clear-all-payments.sql",
@@ -37,16 +34,17 @@ import uk.gov.caz.psr.util.TestObjectFactory.PaymentStatusFactory;
 @AutoConfigureMockMvc
 public class GetChargeSettlementPaymentStatusTestIT {
 
-  private static final String VALID_VRN = "ND84VSX";
-  private static final String VALID_DATE_STRING = "2019-11-01";
+  private static final String NOT_PAID_NOT_EXISTING_DATE_STRING = "2019-10-30";
+  private static final String NOT_PAID_EXISTING_DATE_STRING = "2019-11-03";
+  private static final String REFUNDED_DATE_STRING = "2019-11-02";
+  private static final String PAID_DATE_STRING = "2019-11-01";
+  private static final String PAID_MULTIPLE_PAYMENTS_DATE_STRING = "2019-11-04";
 
-  private static final String VALID_DATE_WITH_STATUSES_WHICH_IS_DIFFERENT_THAN_PAID = "2019-11-02";
-  private static final String VALID_DATE_WITH_DIFFERENT_STATUSES = "2019-11-03";
-  private static final String VALID_DUPLICATED_DATE_STRING = "2019-11-04";
   private static final String VALID_CAZ_ID = "b8e53786-c5ca-426a-a701-b14ee74857d4";
   private static final String VALID_CORRELATION_HEADER = "79b7a48f-27c7-4947-bd1c-670f981843ef";
 
-  private static final String VALID_EXTERNAL_ID = "54321test";
+  private static final String VALID_EXTERNAL_ID_FOR_NOT_PAID = "12345test";
+  private static final String VALID_EXTERNAL_ID_FOR_PAID = "54321test";
   private static final String VALID_CASE_REFERENCE = "case-reference123";
   private static final String PAYMENT_STATUS_GET_PATH = ChargeSettlementController.BASE_PATH +
       ChargeSettlementController.PAYMENT_STATUS_PATH;
@@ -69,26 +67,9 @@ public class GetChargeSettlementPaymentStatusTestIT {
         .header(Headers.TIMESTAMP, LocalDateTime.now())
         .header(Headers.X_API_KEY, VALID_CAZ_ID)
         .param("vrn", nonExistingVrn)
-        .param("dateOfCazEntry", VALID_DATE_STRING))
+        .param("dateOfCazEntry", NOT_PAID_NOT_EXISTING_DATE_STRING))
         .andExpect(status().isOk())
         .andExpect(content().json(getNotPaidResponse()));
-
-  }
-
-  @Test
-  public void shouldReturn500WhenThereAreTwoPaidPaymentStatusesWithSameVrnAndDateOfEntrance()
-      throws Exception {
-    mockMvc.perform(get(PAYMENT_STATUS_GET_PATH)
-        .contentType(MediaType.APPLICATION_JSON)
-        .accept(MediaType.APPLICATION_JSON)
-        .header(Constants.X_CORRELATION_ID_HEADER, VALID_CORRELATION_HEADER)
-        .header(Headers.TIMESTAMP, LocalDateTime.now())
-        .header(Headers.X_API_KEY, VALID_CAZ_ID)
-        .param("vrn", VALID_VRN)
-        .param("dateOfCazEntry", VALID_DUPLICATED_DATE_STRING))
-        .andExpect(status().is5xxServerError())
-        .andExpect(
-            jsonPath("$.errors[0].detail").value("More than one paid VehicleEntrantPayment found"));
   }
 
   @ParameterizedTest
@@ -98,7 +79,7 @@ public class GetChargeSettlementPaymentStatusTestIT {
       "ND 84 VSX", "  ND84V S X ", "N D8   4VSX", // with whitespaces
       "N D8  4v SX " // with whitespaces and changed capitalisation
   })
-  public void shouldReturn200AndPaidPaymentStatusWhenThereAreManyRecordsAndOneOfThemIsPaid(
+  public void shouldReturn200AndPaidPaymentStatusWhenThereIsPaidEntrantExists(
       String vrn)
       throws Exception {
     mockMvc.perform(get(PAYMENT_STATUS_GET_PATH)
@@ -108,10 +89,10 @@ public class GetChargeSettlementPaymentStatusTestIT {
         .header(Headers.TIMESTAMP, LocalDateTime.now())
         .header(Headers.X_API_KEY, VALID_CAZ_ID)
         .param("vrn", vrn)
-        .param("dateOfCazEntry", VALID_DATE_WITH_DIFFERENT_STATUSES))
+        .param("dateOfCazEntry", PAID_DATE_STRING))
         .andExpect(status().isOk())
         .andExpect(content().json(
-            getResponseWith(InternalPaymentStatus.PAID, VALID_CASE_REFERENCE, VALID_EXTERNAL_ID)));
+            getResponseWith(InternalPaymentStatus.PAID, VALID_CASE_REFERENCE, VALID_EXTERNAL_ID_FOR_PAID)));
   }
 
   @ParameterizedTest
@@ -121,7 +102,8 @@ public class GetChargeSettlementPaymentStatusTestIT {
       "ND 84 VSX", "  ND84V S X ", "N D8   4VSX", // with whitespaces
       "N D8  4v SX " // with whitespaces and changed capitalisation
   })
-  public void shouldReturn200AndTheExistingPaymentWhenThereAreNoPaid(String vrn) throws Exception {
+  public void shouldReturn200AndTheExistingPaymentWhenNoPaidEntrantExists(String vrn)
+      throws Exception {
     mockMvc.perform(get(PAYMENT_STATUS_GET_PATH)
         .contentType(MediaType.APPLICATION_JSON)
         .accept(MediaType.APPLICATION_JSON)
@@ -129,11 +111,55 @@ public class GetChargeSettlementPaymentStatusTestIT {
         .header(Headers.TIMESTAMP, LocalDateTime.now())
         .header(Headers.X_API_KEY, VALID_CAZ_ID)
         .param("vrn", vrn)
-        .param("dateOfCazEntry", VALID_DATE_WITH_STATUSES_WHICH_IS_DIFFERENT_THAN_PAID))
+        .param("dateOfCazEntry", NOT_PAID_EXISTING_DATE_STRING))
+        .andExpect(status().isOk())
+        .andExpect(content().json(
+            getResponseWith(InternalPaymentStatus.NOT_PAID, VALID_CASE_REFERENCE,
+                VALID_EXTERNAL_ID_FOR_NOT_PAID)));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "ND84VSX", // existing
+      "Nd84vSX", "nD84vsX", // with changed capitalisation
+      "ND 84 VSX", "  ND84V S X ", "N D8   4VSX", // with whitespaces
+      "N D8  4v SX " // with whitespaces and changed capitalisation
+  })
+  public void shouldReturn200AndTheExistingPaymentWhenIsRefunded(String vrn) throws Exception {
+    mockMvc.perform(get(PAYMENT_STATUS_GET_PATH)
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        .header(Constants.X_CORRELATION_ID_HEADER, VALID_CORRELATION_HEADER)
+        .header(Headers.TIMESTAMP, LocalDateTime.now())
+        .header(Headers.X_API_KEY, VALID_CAZ_ID)
+        .param("vrn", vrn)
+        .param("dateOfCazEntry", REFUNDED_DATE_STRING))
         .andExpect(status().isOk())
         .andExpect(content().json(
             getResponseWith(InternalPaymentStatus.REFUNDED, VALID_CASE_REFERENCE,
-                VALID_EXTERNAL_ID)));
+                VALID_EXTERNAL_ID_FOR_PAID)));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "ND84VSX", // existing
+      "Nd84vSX", "nD84vsX", // with changed capitalisation
+      "ND 84 VSX", "  ND84V S X ", "N D8   4VSX", // with whitespaces
+      "N D8  4v SX " // with whitespaces and changed capitalisation
+  })
+  public void shouldReturn200AndTheLatestPaymentStatus(String vrn) throws Exception {
+    mockMvc.perform(get(PAYMENT_STATUS_GET_PATH)
+        .contentType(MediaType.APPLICATION_JSON)
+        .accept(MediaType.APPLICATION_JSON)
+        .header(Constants.X_CORRELATION_ID_HEADER, VALID_CORRELATION_HEADER)
+        .header(Headers.TIMESTAMP, LocalDateTime.now())
+        .header(Headers.X_API_KEY, VALID_CAZ_ID)
+        .param("vrn", vrn)
+        .param("dateOfCazEntry", PAID_MULTIPLE_PAYMENTS_DATE_STRING))
+        .andExpect(status().isOk())
+        .andExpect(content().json(
+            getResponseWith(InternalPaymentStatus.PAID, VALID_CASE_REFERENCE,
+                VALID_EXTERNAL_ID_FOR_PAID)));
   }
 
   private String getNotPaidResponse() {
