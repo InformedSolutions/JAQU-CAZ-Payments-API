5e731c41d5ebd0d12b11f384f3451e44bb458fa9
cruddasj <james.cruddas@informed.com>
2020-02-17 12:59:07 +0000

    Populate zone ids if no querystring param passed for accounts charge endpoint

--- src/main/java/uk/gov/caz/psr/service/AccountService.java
+++ src/main/java/uk/gov/caz/psr/service/AccountService.java
@@ -1,10 +1,16 @@
 package uk.gov.caz.psr.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import retrofit2.Response;
 import org.springframework.stereotype.Service;
 import uk.gov.caz.psr.dto.AccountVehicleRetrievalResponse;
+import uk.gov.caz.psr.dto.CleanAirZonesResponse;
+import uk.gov.caz.psr.dto.CleanAirZonesResponse.CleanAirZoneDto;
 import uk.gov.caz.psr.repository.AccountsRepository;
+import uk.gov.caz.psr.repository.VccsRepository;
 
 /**
  * Service to interact with the Accounts Service.
@@ -15,6 +21,8 @@ public class AccountService {
   
   private final AccountsRepository accountsRepository;
 
+  private final VccsRepository vccRepository;
+  
   /**
    * Retrieve a page of VRNs of vehicles associated with a given account ID.
    * @param accountId the id of the account
@@ -28,4 +36,20 @@ public class AccountService {
         .body();
   }
 
+  /**
+   * Helper method for retrieving a list of comma delimited clean air zones IDs.
+   * @return a list of comma delimited clean air zones IDs.
+   */
+  public String getZonesQueryStringEquivalent() {
+    Response<CleanAirZonesResponse> zones = vccRepository.findCleanAirZonesSync(); 
+    List<CleanAirZoneDto> cleanAirZoneDtos = zones.body().getCleanAirZones();
+    List<String> mappedZoneIds = new ArrayList<String>();
+
+    for (CleanAirZoneDto dto : cleanAirZoneDtos) {
+      mappedZoneIds.add(dto.getCleanAirZoneId().toString());
+    }
+
+    return String.join(",", mappedZoneIds);
+  }
+  
 }
