82d9bd056789bd7b6cfc6f454e70e852230fa813
Radoslaw Morytko <radoslaw.morytko@informed.com>
2020-07-17 11:44:17 +0200

    NON-JIRA Revert "Temp remove integration test step in dev", clear db before executing any tests from `ChargeSettlementPaymentInfoTestIT`
    This reverts commit 40429c16

--- src/it/java/uk/gov/caz/psr/ChargeSettlementPaymentInfoTestIT.java
+++ src/it/java/uk/gov/caz/psr/ChargeSettlementPaymentInfoTestIT.java
@@ -955,11 +955,11 @@ class ChargeSettlementPaymentInfoTestIT {
   public void insertTestData() {
     // we cannot use SQL annotations on this class, see:
     // https://github.com/spring-projects/spring-framework/issues/19930
+    clearDatabase();
     executeSqlFrom("data/sql/charge-settlement/payment-info/test-data.sql");
   }
 
   @AfterEach
-  @BeforeEach
   public void clearDatabase() {
     executeSqlFrom("data/sql/clear-all-payments.sql");
   }
