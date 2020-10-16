216a254415427a7d9469a8560ff9c57d0a6a2527
Antoni Pstraś <antek.pstras@gmail.com>
2020-10-16 16:18:01 +0200

    [CAZB-3285] Adds paymentMadeDate to ChargeSettlement paymentInfo endpoint

---
+++ src/main/java/uk/gov/caz/psr/service/paymentinfo/PaymentInfoSpecificationByPaymentSubmittedDate.java
@@ -0,0 +1,42 @@
+package uk.gov.caz.psr.service.paymentinfo;
+
+import java.util.Optional;
+import javax.persistence.criteria.Join;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import uk.gov.caz.psr.model.PaymentInfoRequestAttributes;
+import uk.gov.caz.psr.model.info.EntrantPaymentMatchInfo;
+import uk.gov.caz.psr.model.info.EntrantPaymentMatchInfo_;
+import uk.gov.caz.psr.model.info.PaymentInfo;
+import uk.gov.caz.psr.model.info.PaymentInfo_;
+
+/**
+ * Creates specification to filter by payment made date.
+ */
+@Service
+public class PaymentInfoSpecificationByPaymentSubmittedDate implements PaymentInfoSpecification {
+
+  @Override
+  public boolean shouldUse(PaymentInfoRequestAttributes attributes) {
+    return Optional.ofNullable(attributes.getPaymentSubmittedTimestamp()).isPresent();
+  }
+
+  @Override
+  public Specification<EntrantPaymentMatchInfo> create(PaymentInfoRequestAttributes attributes) {
+    return (root, criteriaQuery, criteriaBuilder) -> {
+      Join<EntrantPaymentMatchInfo, PaymentInfo> joinPayment =
+          QueryUtil.getOrCreateJoin(root, EntrantPaymentMatchInfo_.paymentInfo);
+
+      return criteriaBuilder.and(
+          criteriaBuilder.greaterThan(
+              joinPayment.get(PaymentInfo_.submittedTimestamp),
+              attributes.getPaymentSubmittedTimestamp().atStartOfDay()
+          ),
+          criteriaBuilder.lessThan(
+              joinPayment.get(PaymentInfo_.submittedTimestamp),
+              attributes.getPaymentSubmittedTimestamp().plusDays(1).atStartOfDay()
+          )
+      );
+    };
+  }
+}
--- src/test/java/uk/gov/caz/psr/service/ChargeSettlementPaymentInfoServiceTest.java
+++ src/test/java/uk/gov/caz/psr/service/ChargeSettlementPaymentInfoServiceTest.java
@@ -2,6 +2,9 @@ package uk.gov.caz.psr.service;
 
 import static java.util.Collections.emptyList;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -15,8 +18,10 @@ import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.jpa.domain.Specification;
+import uk.gov.caz.psr.controller.exception.PaymentInfoVrnValidationException;
 import uk.gov.caz.psr.model.PaymentInfoRequestAttributes;
 import uk.gov.caz.psr.model.info.EntrantPaymentMatchInfo;
+import uk.gov.caz.psr.repository.EntrantPaymentRepository;
 import uk.gov.caz.psr.repository.jpa.EntrantPaymentMatchInfoRepository;
 import uk.gov.caz.psr.service.paymentinfo.PaymentInfoSpecification;
 
@@ -26,6 +31,9 @@ class ChargeSettlementPaymentInfoServiceTest {
   @Mock
   private EntrantPaymentMatchInfoRepository entrantPaymentMatchInfoRepository;
 
+  @Mock
+  private EntrantPaymentRepository entrantPaymentRepository;
+
   @Spy
   private List<PaymentInfoSpecification> specifications = new ArrayList<>();
 
@@ -33,15 +41,35 @@ class ChargeSettlementPaymentInfoServiceTest {
   private ChargeSettlementPaymentInfoService paymentInfoService;
 
   @Test
-  void shouldReturnListOfPaymentInfo() {
+  public void shouldReturnListOfPaymentInfo() {
     // given
     PaymentInfoRequestAttributes input = PaymentInfoRequestAttributes.builder().build();
-    when(entrantPaymentMatchInfoRepository.findAll(Mockito.any(Specification.class))).thenReturn(emptyList());
+    when(entrantPaymentMatchInfoRepository.findAll(Mockito.any(Specification.class)))
+        .thenReturn(emptyList());
 
     //when
-    List<EntrantPaymentMatchInfo> any = paymentInfoService.findPaymentInfo(input, UUID.randomUUID());
+    List<EntrantPaymentMatchInfo> any = paymentInfoService
+        .findPaymentInfo(input, UUID.randomUUID());
 
     //then
     assertThat(any).isEqualTo(emptyList());
+  }
+
+  @Test
+  public void shouldThrowPaymentInfoVrnValidationExceptionWhenVrnIsMissing() {
+    // given
+    PaymentInfoRequestAttributes input = PaymentInfoRequestAttributes.builder()
+        .vrn("CAS123")
+        .build();
+    when(entrantPaymentRepository.countByVrnAndCaz(any(), anyString())).thenReturn(0);
+
+    // when
+    Throwable throwable = catchThrowable(
+        () -> paymentInfoService.findPaymentInfo(input, UUID.randomUUID()));
+
+    // then
+    assertThat(throwable)
+        .isInstanceOf(PaymentInfoVrnValidationException.class)
+        .hasMessage("vrn cannot be found");
   }
 }
