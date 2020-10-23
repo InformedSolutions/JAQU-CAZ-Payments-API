9496227476079a8f38964ea6d217558ae749f51e
cruddasj <james.cruddas@informed.com>
2020-10-23 15:54:50 +0100

    [CAZB-3457] Add indexes for charge settlement queries
    Add indexes for charge settlement queries

--- src/main/resources/db/changelog/rawSql/0015-1.0-add-indexes-for-charge-settlement-endpoints.sql
+++ src/main/resources/db/changelog/rawSql/0015-1.0-add-indexes-for-charge-settlement-endpoints.sql
@@ -1,8 +1,8 @@
-CREATE INDEX entrant_payment_id ON caz_payment.t_clean_air_zone_entrant_payment_match (clean_air_zone_entrant_payment_id);
+CREATE INDEX IF NOT EXISTS entrant_payment_id ON caz_payment.t_clean_air_zone_entrant_payment_match (clean_air_zone_entrant_payment_id);
 
-CREATE INDEX vrn ON caz_payment.t_clean_air_zone_entrant_payment (vrn);
-CREATE INDEX travel_date ON caz_payment.t_clean_air_zone_entrant_payment (travel_date);
-CREATE INDEX vrn_travel_date ON caz_payment.t_clean_air_zone_entrant_payment (vrn, travel_date);
+CREATE INDEX IF NOT EXISTS vrn ON caz_payment.t_clean_air_zone_entrant_payment (vrn);
+CREATE INDEX IF NOT EXISTS travel_date ON caz_payment.t_clean_air_zone_entrant_payment (travel_date);
+CREATE INDEX IF NOT EXISTS vrn_travel_date ON caz_payment.t_clean_air_zone_entrant_payment (vrn, travel_date);
 
-CREATE INDEX travel_date_vrn_payment_status_caz_id ON caz_payment.t_clean_air_zone_entrant_payment (travel_date, vrn, payment_status, clean_air_zone_id);
-CREATE INDEX caz_id_vrn_travel_date ON caz_payment.t_clean_air_zone_entrant_payment (clean_air_zone_id, vrn, travel_date);
+CREATE INDEX IF NOT EXISTS travel_date_vrn_payment_status_caz_id ON caz_payment.t_clean_air_zone_entrant_payment (travel_date, vrn, payment_status, clean_air_zone_id);
+CREATE INDEX IF NOT EXISTS caz_id_vrn_travel_date ON caz_payment.t_clean_air_zone_entrant_payment (clean_air_zone_id, vrn, travel_date);
