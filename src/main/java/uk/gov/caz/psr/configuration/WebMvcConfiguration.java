20072253e31b6fb07e815a63907bcd8e7b475301
jenniferbrown94 <jennifer.brown@informed.com>
2020-10-27 13:54:51 +0000

    add comments

--- src/main/java/uk/gov/caz/psr/configuration/WebMvcConfiguration.java
+++ src/main/java/uk/gov/caz/psr/configuration/WebMvcConfiguration.java
@@ -5,10 +5,15 @@ import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 class WebMvcConfiguration implements WebMvcConfigurer {
+  /**
+  * override method to set default content type to Json
+  */
   @Override
   public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+	// set default content type for responses to Json
     configurer.defaultContentType(MediaType.APPLICATION_JSON);
   }
 }
