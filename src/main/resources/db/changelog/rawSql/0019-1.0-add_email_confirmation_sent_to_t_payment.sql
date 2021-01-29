5c8633fd755f1eee81c45723f1e189559bcd181e
Antoni Pstraś <antek.pstras@gmail.com>
2021-01-29 13:58:55 +0100

    [CAZB-3905] email_confirmation_sent population fix

--- src/main/resources/db/changelog/rawSql/0019-1.0-add_email_confirmation_sent_to_t_payment.sql
+++
@@ -1,5 +0,0 @@
--- Add `email_confirmation_sent` attribute to T_PAYMENT table:
-ALTER TABLE CAZ_PAYMENT.t_payment ADD COLUMN email_confirmation_sent boolean DEFAULT false NOT NULL;
-
--- Set email_confirmation_sent=true for already existing records:
-UPDATE CAZ_PAYMENT.t_payment SET email_confirmation_sent = true;
