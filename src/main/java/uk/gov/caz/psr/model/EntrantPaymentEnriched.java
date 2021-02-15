3acc51155d1aa76c798a65c2ec6de221f6d03d87
Antoni <a.pstras@exlabs.pl>
2021-02-15 10:48:25 +0100

    [CAZB-3912] isRefunded and isChargedback added to payment history response (#459)

--- src/main/java/uk/gov/caz/psr/model/EntrantPaymentEnriched.java
+++ src/main/java/uk/gov/caz/psr/model/EntrantPaymentEnriched.java
@@ -1,12 +1,16 @@
 package uk.gov.caz.psr.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Preconditions;
 import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Stream;
 import lombok.Builder;
 import lombok.Value;
 import uk.gov.caz.psr.model.info.EntrantPaymentMatchInfo;
@@ -16,6 +20,7 @@ import uk.gov.caz.psr.model.info.EntrantPaymentMatchInfo;
  */
 @Value
 @Builder(toBuilder = true)
+@JsonIgnoreProperties({"refunded", "chargedback"})
 public class EntrantPaymentEnriched {
 
   @ApiModelProperty(value = "${swagger.model.descriptions.historical-payment.travelDate}")
@@ -41,14 +46,23 @@ public class EntrantPaymentEnriched {
   @ApiModelProperty(value = "${swagger.model.descriptions.historical-payment.paymentStatus}")
   ExternalPaymentStatus paymentProviderStatus;
 
+  @ApiModelProperty(value = "${swagger.model.descriptions.historical-payment.isRefunded")
+  @JsonProperty(value = "isRefunded")
+  boolean isRefunded;
+
+  @ApiModelProperty(value = "${swagger.model.descriptions.historical-payment.isChargedback")
+  @JsonProperty(value = "isChargedback")
+  boolean isChargedback;
+
   /**
    * Function constructing entity based on database entities.
+   *
    * @param matchInfo entity keeping info on entrant.
    * @param cazIdMap map of CAZ ids and its respective names.
    * @return this entity.
    */
   public static EntrantPaymentEnriched fromMatchInfo(EntrantPaymentMatchInfo matchInfo,
-      Map<UUID, String> cazIdMap) {
+      Map<UUID, String> cazIdMap, List<PaymentStatusAuditData> auditData) {
     Preconditions.checkNotNull(matchInfo, "match object should be not-null");
 
     return EntrantPaymentEnriched.builder()
@@ -59,7 +73,40 @@ public class EntrantPaymentEnriched {
         .paymentId(matchInfo.getPaymentInfo().getId())
         .paymentReference(matchInfo.getPaymentInfo().getReferenceNumber())
         .paymentProviderStatus(matchInfo.getPaymentInfo().getExternalPaymentStatus())
+        .isRefunded(isEntrantPaymentRefunded(matchInfo, auditData))
+        .isChargedback(isEntrantPaymentChargedBack(matchInfo, auditData))
         .build();
   }
 
+  /**
+   * Returns information if a given entrantPayment was charged back.
+   */
+  private static boolean isEntrantPaymentChargedBack(EntrantPaymentMatchInfo matchInfo,
+      List<PaymentStatusAuditData> auditData) {
+    return getFilteredAuditData(matchInfo, auditData)
+        .anyMatch(e -> e.getPaymentStatus().equals(InternalPaymentStatus.CHARGEBACK));
+  }
+
+  /**
+   * Returns information if a given entrantPayment was refunded.
+   */
+  private static boolean isEntrantPaymentRefunded(EntrantPaymentMatchInfo matchInfo,
+      List<PaymentStatusAuditData> auditData) {
+    return getFilteredAuditData(matchInfo, auditData)
+        .anyMatch(e -> e.getPaymentStatus().equals(InternalPaymentStatus.REFUNDED));
+  }
+
+  /**
+   * Method performs a filtering on the provided {@code List<PaymentStatusAuditData} auditData using
+   * the {@link EntrantPaymentMatchInfo}.
+   */
+  private static Stream<PaymentStatusAuditData> getFilteredAuditData(
+      EntrantPaymentMatchInfo matchInfo, List<PaymentStatusAuditData> auditData) {
+    return auditData
+        .stream()
+        .filter(e ->
+            e.getCleanAirZoneId().equals(matchInfo.getEntrantPaymentInfo().getCleanAirZoneId())
+                && e.getPaymentId().equals(matchInfo.getPaymentInfo().getId())
+                && e.getTravelDate().equals(matchInfo.getEntrantPaymentInfo().getTravelDate()));
+  }
 }
