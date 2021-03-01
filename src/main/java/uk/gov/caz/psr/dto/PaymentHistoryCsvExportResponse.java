0a1c7ff269e6aed7785c9fcc451c1c05fcec63aa
Antoni <a.pstras@exlabs.pl>
2021-03-01 09:45:31 +0100

    [CAZB-4138] CSV export endpoint (#477)

---
+++ src/main/java/uk/gov/caz/psr/dto/PaymentHistoryCsvExportResponse.java
@@ -0,0 +1,25 @@
+package uk.gov.caz.psr.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Value;
+
+/**
+ * Value object that represents details of an exported csv file.
+ */
+@Value
+@Builder
+public class PaymentHistoryCsvExportResponse {
+
+  /**
+   * String containing the url to the s3 file.
+   */
+  @ApiModelProperty(value = "swagger.model.descriptions.payment-history-csv-export.fileUrl")
+  String fileUrl;
+
+  /**
+   * String containing the bucket name.
+   */
+  @ApiModelProperty(value = "swagger.model.descriptions.payment-history-csv-export.bucketName")
+  String bucketName;
+}
