bb6b70e08b728cfc6be5e5130de568f4915c5b15
jenniferbrown94 <jennifer.brown@informed.com>
2021-03-11 16:18:04 +0000

    fix query

--- src/main/resources/db/changelog/rawSql/0025-1.0-reporting-number-of-individual-vehicles-paid-for.sql
+++ src/main/resources/db/changelog/rawSql/0025-1.0-reporting-number-of-individual-vehicles-paid-for.sql
@@ -7,5 +7,4 @@ CREATE OR REPLACE VIEW caz_reporting.number_of_individual_vehicles_paid_for AS
     INNER JOIN 
     caz_payment.t_payment AS pay
     ON payment_match.payment_id = pay.payment_id
-  WHERE payment_authorised_timestamp >= '2021-03-11'
   GROUP BY payment_authorised_timestamp, payment_provider_status
