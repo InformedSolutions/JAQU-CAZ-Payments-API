897a4a557f5b5842564b2598f1a2603272a9b248
jenniferbrown94 <44293392+jenniferbrown94@users.noreply.github.com>
2021-03-12 09:36:21 +0000

    [CAZB-3757] Add view for number of individual vehicles paid for to reporting schema (#485)
    * add reporting to schema
    
    * fix query

---
+++ src/main/resources/db/changelog/rawSql/0025-1.0-reporting-number-of-individual-vehicles-paid-for.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE VIEW caz_reporting.number_of_individual_vehicles_paid_for AS
+  SELECT payment_authorised_timestamp, 
+  payment_provider_status, 
+  count(*) as no_vehicles_paid_for 
+  FROM 
+    caz_payment.t_clean_air_zone_entrant_payment_match AS payment_match
+    INNER JOIN 
+    caz_payment.t_payment AS pay
+    ON payment_match.payment_id = pay.payment_id
+  GROUP BY payment_authorised_timestamp, payment_provider_status
