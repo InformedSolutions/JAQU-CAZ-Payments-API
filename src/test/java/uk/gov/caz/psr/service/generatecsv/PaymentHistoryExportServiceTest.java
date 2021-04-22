75fe38943ee403598a56e4241dc58a484218d2f3
Sergii Chub <s.chub@exlabs.pl>
2021-04-22 18:35:49 +0200

    Add tests

--- src/test/java/uk/gov/caz/psr/service/generatecsv/PaymentHistoryExportServiceTest.java
+++ src/test/java/uk/gov/caz/psr/service/generatecsv/PaymentHistoryExportServiceTest.java
@@ -1,5 +1,7 @@
 package uk.gov.caz.psr.service.generatecsv;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
@@ -11,6 +13,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 import lombok.SneakyThrows;
+import okhttp3.MediaType;
+import okhttp3.ResponseBody;
 import org.apache.commons.lang3.RandomUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,6 +25,7 @@ import retrofit2.Response;
 import uk.gov.caz.psr.dto.PaymentsHistoryLambdaInput;
 import uk.gov.caz.psr.dto.accounts.UpdatePaymentHistoryExportRequest;
 import uk.gov.caz.psr.repository.AccountsRepository;
+import uk.gov.caz.psr.service.exception.ExternalServiceCallException;
 
 @ExtendWith(MockitoExtension.class)
 public class PaymentHistoryExportServiceTest {
@@ -44,7 +49,10 @@ public class PaymentHistoryExportServiceTest {
   @Test
   public void shouldUploadFileAndCallAccountsApi() {
     //given
-    mockServices();
+    UpdatePaymentHistoryExportRequest request = mockUpdatePaymentHistoryExportRequest();
+    when(accountsRepository.updatePaymentHistoryExportJobSync(
+        ANY_ACCOUNT_ID, ANY_REGISTER_JOB_ID, request))
+        .thenReturn(Response.success(null));
 
     //when
     paymentHistoryExportService.execute(mockPaymentsHistoryLambdaInput());
@@ -54,6 +62,43 @@ public class PaymentHistoryExportServiceTest {
         eq(ANY_ACCOUNT_ID), eq(ANY_REGISTER_JOB_ID), any());
   }
 
+  @Test
+  public void shouldThrowExternalServiceCallExceptionOnUnsuccessfulResponse()
+      throws MalformedURLException {
+    //given
+    UpdatePaymentHistoryExportRequest request = mockUpdatePaymentHistoryExportRequest();
+    when(accountsRepository.updatePaymentHistoryExportJobSync(
+        ANY_ACCOUNT_ID, ANY_REGISTER_JOB_ID, request))
+        .thenReturn(Response.error(500,
+            ResponseBody.create(MediaType.get("application/json"), "")));
+
+    //when
+    Throwable throwable = catchThrowable(() -> paymentHistoryExportService
+        .execute(mockPaymentsHistoryLambdaInput()));
+
+    // then
+    assertThat(throwable).isInstanceOf(ExternalServiceCallException.class);
+  }
+
+  private UpdatePaymentHistoryExportRequest mockUpdatePaymentHistoryExportRequest()
+      throws MalformedURLException {
+    URL fileUrl = mockUrl();
+    when(paymentsHistoryCsvFileSupervisor.uploadCsvFileAndGetPresignedUrl(
+        ANY_ACCOUNT_ID, ANY_ACCOUNT_USER_IDS)).thenReturn(fileUrl);
+    return mockPaymentHistoryExportRequest(fileUrl);
+  }
+
+  private URL mockUrl() throws MalformedURLException {
+    return new URL("https://return-url.com");
+  }
+
+  private UpdatePaymentHistoryExportRequest mockPaymentHistoryExportRequest(URL fileUrl) {
+    return UpdatePaymentHistoryExportRequest.builder()
+        .fileUrl(fileUrl)
+        .status("FINISHED_SUCCESS")
+        .build();
+  }
+
   private PaymentsHistoryLambdaInput mockPaymentsHistoryLambdaInput() {
     return PaymentsHistoryLambdaInput.builder()
         .correlationId(ANY_CORRELATION_ID)
@@ -61,21 +106,5 @@ public class PaymentHistoryExportServiceTest {
         .accountId(ANY_ACCOUNT_ID)
         .accountUserIds(ANY_ACCOUNT_USER_IDS)
         .build();
-  }
-
-  private void mockServices() throws MalformedURLException {
-    URL fileUrl = new URL("https://return-url.com");
-
-    when(paymentsHistoryCsvFileSupervisor.uploadCsvFileAndGetPresignedUrl(
-        ANY_ACCOUNT_ID, ANY_ACCOUNT_USER_IDS)).thenReturn(fileUrl);
-
-    UpdatePaymentHistoryExportRequest request = UpdatePaymentHistoryExportRequest.builder()
-        .fileUrl(fileUrl)
-        .status("FINISHED_SUCCESS")
-        .build();
-
-    when(accountsRepository.updatePaymentHistoryExportJobSync(
-        ANY_ACCOUNT_ID, ANY_REGISTER_JOB_ID, request))
-        .thenReturn(Response.success(null));
   }
 }
