a7fa2470a07ef7c13fa50ca96d5cd715415e5792
lukaszkuczynski <kuczynskilukasz@gmail.com>
2021-06-02 08:20:04 +0200

    [CAZB-4285] Update vehicle history endpoint with `isModifed` flag.

---
+++ src/it/resources/data/sql/perform-la-update-to-failed.sql
@@ -0,0 +1,7 @@
+UPDATE caz_payment.t_clean_air_zone_entrant_payment
+  SET update_actor = 'LA', payment_status = 'FAILED'
+  WHERE vrn = 'ND84VSX' AND clean_air_zone_id = 'b8e53786-c5ca-426a-a701-b14ee74857d4' AND travel_date = '2019-11-01';
+
+UPDATE caz_payment.t_clean_air_zone_entrant_payment
+  SET update_actor = 'LA', payment_status = 'PAID'
+  WHERE vrn = 'ND84VSX' AND clean_air_zone_id = 'b8e53786-c5ca-426a-a701-b14ee74857d4' AND travel_date = '2019-11-01';
--- src/main/java/uk/gov/caz/psr/model/InternalPaymentStatus.java
+++ src/main/java/uk/gov/caz/psr/model/InternalPaymentStatus.java
@@ -1,5 +1,9 @@
 package uk.gov.caz.psr.model;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * An internal payment status as specified in ICD.
  */
@@ -19,4 +23,9 @@ public enum InternalPaymentStatus {
     }
     return NOT_PAID;
   }
+
+  public static Set<InternalPaymentStatus> modifiedStatuses() {
+    return new HashSet<>(Arrays.asList(REFUNDED, CHARGEBACK, FAILED));
+  }
+
 }
